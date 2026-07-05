c338ce3a70d66ff3a22cfd700a51bc6f93e62ce6
supbose <yshxw@qq.com>
2026-07-05 22:16:11 +0800

    chore: bump version for default to v2.3.3

--- test-changelog.ts
+++
@@ -1,7 +0,0 @@
-import { ChangelogManager } from './src/core/ChangelogManager'
-
-const cm = new ChangelogManager('./rust-test')
-cm.updateChangelog('0.0.11', [
-  { hash: 'test-hash-123', message: 'feat: test commit', files: ['Cargo.toml'] },
-])
-console.log('Changelog updated')
--- test-git2.ts
+++
@@ -1,5 +0,0 @@
-import { GitManager } from './src/core/GitManager'
-
-const gm = new GitManager('./rust-test')
-const lastTag = gm.getLastTag()
-console.log('Last tag:', lastTag)
