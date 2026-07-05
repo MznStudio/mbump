e4f8fc76e0563a8e29908adce524fe37afcdd008
supbose <yshxw@qq.com>
2026-07-05 22:05:37 +0800

    ci(github workflows): 发布npm时跳过git检查

--- test-git.ts
+++
@@ -1,5 +0,0 @@
-import { GitManager } from './src/core/GitManager'
-
-const gm = new GitManager('./rust-test')
-const commits = gm.getCommitsSinceLastTag()
-console.log('Commits:', JSON.stringify(commits, null, 2))
